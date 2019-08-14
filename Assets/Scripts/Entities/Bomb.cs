32805582d66d720e6b0d75f78fee629df661c6da
Jack <47583484+JackADV@users.noreply.github.com>
2019-08-14 16:08:57 +1000

    First Commit

---
+++ Assets/Scripts/Entities/Bomb.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Mirror;
+
+public class Bomb : NetworkBehaviour
+{
+    public float explosionRadius = 2f;
+    public float explosionDelay = 2f;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
+
+    public IEnumerator Explode()
+    {
+        yield return new WaitForSeconds(explosionDelay);
+
+        Collider[] hits = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (var hit in hits)
+        {
+            NetworkServer.Destroy(hit.gameObject);
+        }
+
+    }
+    [Command]
+    void CmdExplode(Vector3 position, float radius)
+    {
+        Collider[] hits = Physics.OverlapSphere(position, radius);
+
+        foreach (var hit in hits)
+        {
+            NetworkServer.Destroy(hit.gameObject);
+        }
+
+    }
+    void Start()
+    {
+        StartCoroutine(Explode());
+    }
+}
+
